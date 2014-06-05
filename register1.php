afe140b97bc14f750d5b424ce2a025ebee978505
dtool <pojar.andrei@gmail.com>
2014-06-05 13:45:30 +0300

    hotel data base
    Better hotel table

--- register1.php
+++ register1.php
@@ -56,7 +56,7 @@ mysql_connect("$host", "$db_username", "$db_password")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 	
 
-	$firstName;
+echo	$firstName;
 $address; 
 $city; 
 $cnp = 
